e4c8394f2655d121b1dafe76bb28dfd838eb1b53
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-09-12 19:18:39 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold sm:bg-slate-500 md:bg-black lg:bg-violet-500 underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
